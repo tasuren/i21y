297d0f019c4ca237d520d276c369b42761a1e728
Takagi Tasuku <tasuren@outlook.jp>
2023-08-24 12:12:33 +0900

    バージョンの更新が不適切であったのを修正。

--- src/i21y/__init__.py
+++ src/i21y/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Takagi Tasuku"
-__version__ = "0.2.2"
+__version__ = "0.2.0"
 __all__ = ("Translator", "I21YError", "TranslationNotFound", "locale_str")
 
 from .translator import Translator
