547dc272f971616d27a9de49331a83ba6cb57356
Takagi Tasuku <tasuren@outlook.jp>
2026-08-11 10:20:38 +0900

    build: bump i21y version to 0.4.2

--- src/i21y/__init__.py
+++ src/i21y/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Takagi Tasuku"
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __all__ = ("I21YError", "TranslationNotFound", "Translator", "locale_str")
 
 from .error import I21YError, TranslationNotFound
